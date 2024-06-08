09b27ce12a9e81289c58f0e25a458e8edfd591f6
kuan08710 <kkuan1020@gmail.com>
2024-06-08 20:16:01 +0800

    商品功能 - 刪除商品

--- springboot-mall/src/main/java/com/louis/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/louis/springbootmall/controller/ProductController.java
@@ -60,4 +60,11 @@ public class ProductController {
                              .body(updateProduct);
     }
 
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct (@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                             .build();
+    }
 }
