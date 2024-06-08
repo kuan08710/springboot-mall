243a766c828cd4f44009218e456067776817a079
kuan08710 <kkuan1020@gmail.com>
2024-06-08 18:17:10 +0800

    商品功能 - Refactor category Enum

---
+++ springboot-mall/src/main/java/com/louis/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,5 @@
+package com.louis.springbootmall.constant;
+
+public enum ProductCategory {
+    
+}
--- springboot-mall/src/main/java/com/louis/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/louis/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.louis.springbootmall.rowmapper;
 
+import com.louis.springbootmall.constant.ProductCategory;
 import com.louis.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,13 +15,14 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId (resultSet.getInt ("product_id"));
         product.setProductName (resultSet.getString ("product_name"));
-        product.setCategory (resultSet.getString ("category"));
+        product.setCategory (ProductCategory.valueOf (resultSet.getString ("category")));
         product.setImageUrl (resultSet.getString ("image_url"));
         product.setPrice (resultSet.getInt ("price"));
         product.setStock (resultSet.getInt ("stock"));
         product.setDescription (resultSet.getString ("category"));
         product.setCreatedDate (resultSet.getTimestamp ("created_date"));
         product.setLastModifieDate (resultSet.getTimestamp ("last_modified_date"));
+
         return product;
     }
 }
